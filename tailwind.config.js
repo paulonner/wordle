093ce2b1dc849ac080756e82f665ae0285b5986d
Paulo MR <paulosergioricardez@gmail.com>
2024-02-29 08:50:20 -0600

    tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#F3F3F3',
+          100: '#DADCE0',
+          200: '#939B9F',
+          300: '#D3D6DA',
+          400: '#818181',
+          500: '#939B9F'
+        },
+        yellow: {
+          500: '#CEB02C'
+        },
+        green: {
+          500: '#66A060',
+          600: '#6AAA64'
+        }
+      }
+    },
   },
   plugins: [],
 }
